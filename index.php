3f96d18a09681bf7dc630eae1dffde5ca62828b0
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2022-06-02 18:56:42 +0900

    final layout

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <title>SaCode PHP Batch 1 Day 04</title>
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <div class="container">
+
+            <!-- HEADER -->
+            <div class="row">
+                <div class="col-12">
+                <h1>HEADER</h1>
+                <p>Your tag line here</p>
+                </div>
+            </div>
+            <!-- END HEADER -->
+
+            <!-- NAV -->
+            <div class="row bg-dark p-3">
+                <div class="col-12">
+                    
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Contact</a>
+                    </li>
+                </ul>
+
+                </div>
+                
+            </div>
+            <!-- END NAV -->
+
+            <!-- CONTENT -->
+            <div class="row">
+                <div class="col-3 border p-3">
+                    SIDEBAR
+                </div>
+                <div class="col-9 border p-3">
+                    MAIN CONTENT
+                </div>
+            </div>
+            <!-- END CONTENT -->
+
+            <!-- FOOTER -->
+            <div class="row bg-dark p-3 text-light">
+                <div class="col-12">
+                    FOOTER
+                </div>
+            </div>
+            <!-- END FOOTER -->
+
+        </div>
+        
+    </body>
+</html>
